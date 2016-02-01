2ef1db87e891886e322a321d7a223f363bf686fd
Jan Florian Dietrich <jfd@daenen4.de>
2016-02-01 13:09:49 +0100

    chore(setup): Add basic webpack, babel, react setup

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const merge = require('webpack-merge')
+const path = require('path')
+const webpack = require('webpack')
+
+const TARGET = process.env.npm_lifecycle_event
+process.env.BABEL_ENV = TARGET
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+}
+
+const common = {
+  entry: PATHS.app,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'svo.js'
+  }
+}
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST || 'localhost',
+      port: process.env.PORT || 8080
+    },
+    devtool: 'eval-source-map',
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  })
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {})
+}
