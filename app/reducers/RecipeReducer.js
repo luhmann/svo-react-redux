c8844fc65a29e011f1d7158332426cf3397747ae
Jan Florian Dietrich <jfd@daenen4.de>
2016-02-05 18:16:45 +0100

    feat: keep reference config for immutable save

--- app/reducers/RecipeReducer.js
+++ app/reducers/RecipeReducer.js
@@ -1,20 +1,20 @@
 import { REQUEST_RECIPE, RECEIVE_RECIPE } from '../constants/ActionTypes.js'
-import { Map } from 'immutable'
+import { Record } from 'immutable'
 
-const intialState = Map({
+const intialState = new Record({
   fetching: false,
   current: null
-})
+})()
 
 function recipe (state = intialState, action) {
   switch (action.type) {
     case REQUEST_RECIPE:
       return state.set('fetching', true)
     case RECEIVE_RECIPE:
-      return Map({
+      return new Record({
         fetching: false,
         current: action.recipe
-      })
+      })()
   }
 
   return state
