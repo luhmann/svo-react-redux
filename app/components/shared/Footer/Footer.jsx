d7e8e713bd8fc2d8fd96eaf1a6915417640b4c5c
Jan Florian Dietrich <jfd@daenen4.de>
2016-09-10 00:32:05 +0200

    fix: first step to getting tests back

--- app/components/shared/Footer/Footer.jsx
+++ app/components/shared/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import { React, CSSModules, CSSModuleConfig } from 'lib/commonImports.js'
 import styles from './Footer.styl'
 
 const Footer = () => (
-  <footer styleName='footer'>
-    Footer
+  <footer styleName='root'>
+    &copy; 2016 Stove vs. Oven
   </footer>
 )
 
